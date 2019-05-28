af2ec07caed378b0f3b937a9cc20b7ed8a4b2d53
Stephen <stephen@remine.com>
2019-05-28 12:33:30 -0500

    Change favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -88,7 +88,7 @@ module.exports = {
         background_color: `#ffffff`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `content/assets/gatsby-icon.png`,
+        icon: `content/assets/all_inclusive.svg`,
         query: `
           {
             site {
