1abba4a844bdf033631a5fbf5097e445e6316af5
Stephen <stephen@remine.com>
2019-05-29 20:44:42 -0500

    add full text search

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -2,6 +2,10 @@ import React, { Component } from 'react'
 import { Link } from 'gatsby'
 import { Index } from 'elasticlunr'
 
+function getBlurb(page) {
+  return page.html.slice(0, 500).concat('...')
+}
+
 // Search component
 export default class Search extends Component {
   constructor(props) {
@@ -12,9 +16,30 @@ export default class Search extends Component {
     }
   }
 
+  getOrCreateIndex = () =>
+    this.index
+      ? this.index
+      : // Create an elastic lunr index and hydrate with graphql query results
+        Index.load(this.props.searchIndex)
+
+  search = e => {
+    const query = e.target.value
+    this.index = this.getOrCreateIndex()
+
+    this.setState({
+      query,
+      // Query the index with search string to get an [] of IDs
+      results: this.index
+        .search(query, { expand: true })
+        // Map over each ID and return the full document
+        .map(({ ref }) => this.index.documentStore.getDoc(ref)),
+    })
+  }
+
+  handleQuery = e => {
+    this.search(e)
+  }
   render() {
-    const { results, query } = this.state
-    console.log({ results, query })
     return (
       <div>
         <div
@@ -30,42 +55,37 @@ export default class Search extends Component {
             style={{ width: '100%' }}
             type="text"
             value={this.state.query}
-            onChange={this.search}
+            onChange={this.handleQuery}
           />
         </div>
         <ul>
           {this.state.results &&
-            this.state.results.map(page => (
-              <li key={page.id}>
-                <div style={{ display: 'flex', flexDirection: 'column' }}>
-                  <Link to={'/' + page.path}>{page.title}</Link>
-                  <div style={{ 'margin-left': '1em' }}>
-                    {' Tags: ' + page.tags.join(`, `)}
+            this.state.results.map(page => {
+              const blurb = getBlurb(page)
+              return (
+                <li key={page.id}>
+                  <div
+                    style={{
+                      display: 'flex',
+                      flexDirection: 'column',
+                    }}
+                  >
+                    <Link to={'/' + page.path}>{page.title}</Link>
+                    <div
+                      style={{
+                        background: 'rgba(0,0,0,.1)',
+                        marginLeft: '1em',
+                      }}
+                    >
+                      {page.tags && ' Tags: ' + page.tags.join(`, `)}
+                    </div>
+                    <div style={{ marginLeft: '1em' }}>{blurb && blurb}</div>
                   </div>
-                </div>
-              </li>
-            ))}
+                </li>
+              )
+            })}
         </ul>
       </div>
     )
-  }
-  getOrCreateIndex = () =>
-    this.index
-      ? this.index
-      : // Create an elastic lunr index and hydrate with graphql query results
-        Index.load(this.props.searchIndex)
-
-  search = evt => {
-    const query = evt.target.value
-    this.index = this.getOrCreateIndex()
-
-    this.setState({
-      query,
-      // Query the index with search string to get an [] of IDs
-      results: this.index
-        .search(query, { expand: true })
-        // Map over each ID and return the full document
-        .map(({ ref }) => this.index.documentStore.getDoc(ref)),
-    })
   }
 }
