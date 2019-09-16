591ea91bcfeb73a33596e7a32d411856397489f0
Stephen Weiss <stephencweiss@gmail.com>
2019-09-16 08:55:08 -0500

    Feature/add analyze script (#120)
    * entry/2019-08-20
    
    * Revert "entry/2019-08-20"
    
    This reverts commit ae38aa61b14e8ced0b67e17511818dd87f46022a.
    
    * Adding an analyze script to see what I'm shipping to the client

--- gatsby-config.js
+++ gatsby-config.js
@@ -168,5 +168,14 @@ module.exports = {
         },
       },
     },
+    {
+      resolve: 'gatsby-plugin-webpack-bundle-analyzer',
+      options: {
+        production: true,
+        disable: !process.env.ANALYZE_BUNDLE_SIZE,
+        generateStatsFile: true,
+        analyzerMode: 'static',
+      }
+    }
   ],
 }
