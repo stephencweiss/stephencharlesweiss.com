d15af5ff4e7968cf86796d2069d5cf71c0537862
Stephen <stephen@remine.com>
2019-10-21 08:26:54 -0400

    simplify logic on create page

--- gatsby-node.js
+++ gatsby-node.js
@@ -40,29 +40,19 @@ exports.createPages = ({ graphql, actions }) => {
     const posts = result.data.allMarkdownRemark.edges
     posts.forEach((post, index) => {
       const previous = index === posts.length - 1 ? null : posts[index + 1].node
-      const next =
-        index === 0
-          ? null
-          : posts[index - 1].node
-          ? BUILD_TIME.isAfter(publish ? publish : date)
-          ?  createPage({
-              path: post.node.fields.slug,
-              component: unpublishedPost,
-              context: {
-                slug: post.node.fields.slug,
-                previous,
-                next,
-              },
-            })
-          : createPage({
-              path: post.node.fields.slug,
-              component: blogPost,
-              context: {
-                slug: post.node.fields.slug,
-                previous,
-                next,
-              },
-            })
+      const next = index === 0 ? null : posts[index - 1].node
+
+      createPage({
+        path: post.node.fields.slug,
+        component: BUILD_TIME.isAfter(publish ? publish : date)
+          ? unpublishedPost
+          : blogPost,
+        context: {
+          slug: post.node.fields.slug,
+          previous,
+          next,
+        },
+      })
     })
   })
 }
