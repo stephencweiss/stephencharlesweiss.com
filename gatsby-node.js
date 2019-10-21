69ee71f8b168127653f08531e78185afe1cc14ff
Stephen <stephen@remine.com>
2019-10-21 08:26:44 -0400

    choose page based on build time

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,10 +1,14 @@
 const path = require(`path`)
+const dayjs = require('dayjs')
 const { createFilePath } = require(`gatsby-source-filesystem`)
+
+const BUILD_TIME = dayjs()
 
 exports.createPages = ({ graphql, actions }) => {
   const { createPage } = actions
 
   const blogPost = path.resolve(`./src/templates/blog-post.js`)
+  const unpublishedPost = path.resolve(`./src/templates/unpublished-post.js`)
   return graphql(
     `
       {
@@ -19,6 +23,8 @@ exports.createPages = ({ graphql, actions }) => {
               }
               frontmatter {
                 title
+                date
+                publish
               }
             }
           }
@@ -34,17 +40,29 @@ exports.createPages = ({ graphql, actions }) => {
     const posts = result.data.allMarkdownRemark.edges
     posts.forEach((post, index) => {
       const previous = index === posts.length - 1 ? null : posts[index + 1].node
-      const next = index === 0 ? null : posts[index - 1].node
-
-      createPage({
-        path: post.node.fields.slug,
-        component: blogPost,
-        context: {
-          slug: post.node.fields.slug,
-          previous,
-          next,
-        },
-      })
+      const next =
+        index === 0
+          ? null
+          : posts[index - 1].node
+          ? BUILD_TIME.isAfter(publish ? publish : date)
+          ?  createPage({
+              path: post.node.fields.slug,
+              component: unpublishedPost,
+              context: {
+                slug: post.node.fields.slug,
+                previous,
+                next,
+              },
+            })
+          : createPage({
+              path: post.node.fields.slug,
+              component: blogPost,
+              context: {
+                slug: post.node.fields.slug,
+                previous,
+                next,
+              },
+            })
     })
   })
 }
