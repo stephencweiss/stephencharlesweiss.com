df13f36b3b841ace1539d7406358a3402a9ad39c
Stephen <stephen@remine.com>
2019-10-26 18:55:36 -0500

    WIP - search with debounce

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,6 +1,23 @@
-import React, { Component } from 'react'
+import React, { Component, useState, useEffect } from 'react'
 import { Link } from 'gatsby'
 import { Index } from 'elasticlunr'
+
+// underscoreJS Debounce via David Walsh - https://davidwalsh.name/javascript-debounce-function
+function debounce(func, wait, immediate) {
+  var timeout
+  return function() {
+    var context = this,
+      args = arguments
+    var later = function() {
+      timeout = null
+      if (!immediate) func.apply(context, args)
+    }
+    var callNow = immediate && !timeout
+    clearTimeout(timeout)
+    timeout = setTimeout(later, wait)
+    if (callNow) func.apply(context, args)
+  }
+}
 
 function getBlurb(page) {
   return page.html.slice(0, 500).concat('...')
@@ -39,6 +56,7 @@ export default class Search extends Component {
   handleQuery = e => {
     this.search(e)
   }
+
   render() {
     return (
       <div>
@@ -89,3 +107,101 @@ export default class Search extends Component {
     )
   }
 }
+
+//
+// const Search = function Search(props) {
+//   const [query, setQuery] = useState('')
+//   const [results, setResults] = useState([])
+//   //   const [index, setIndex] = useState({})
+//   console.log({ props })
+//   const { searchIndex } = props
+
+//   useEffect(() => {
+//     if (!query) {
+//       setResults([])
+//       return
+//     }
+
+//     const lunrIndex = Index.build(searchIndex)
+//     console.log({ lunrIndex })
+//     // const searchResults = lunrIndex.index.search(query, { expand: true })
+//     console.log(lunrIndex.index, lunrIndex.index.search)
+//     // setResults(searchResults.map(({ ref }) => lunrIndex.store[ref]))
+//   }, [query])
+
+//   //   const getOrCreateIndex = () =>
+//   //     index
+//   //       ? index
+//   //       : // Create an elastic lunr index and hydrate with graphql query results
+//   //         setIndex(Index.load(searchIndex))
+
+//   //   const search = e => {
+//   //     console.log(e, e.target, e.target.value)
+//   //     const query = e.target.value
+//   //     Object.keys(index).length === 0 && getOrCreateIndex()
+
+//   //     setQuery(query)
+//   //     setResults(
+//   //       index
+//   //         .search(query, { expand: true })
+//   //         // Map over each ID and return the full document
+//   //         .map(({ ref }) => index.documentStore.getDoc(ref))
+//   //     )
+//   //   }
+
+//   //   const handleQuery = e => {
+//   //     search(e)
+//   //   }
+
+//   const handleQuery = event => setQuery(event.target.value)
+
+//   return (
+//     <div>
+//       <div
+//         style={{
+//           display: 'flex',
+//           flex: 1,
+//           flexDirection: 'row',
+//           alignItems: 'center',
+//         }}
+//       >
+//         Search:{' '}
+//         <input
+//           style={{ width: '100%' }}
+//           type="text"
+//           value={query}
+//           onChange={handleQuery}
+//         />
+//       </div>
+//       <ul>
+//         {results &&
+//           results.map(page => {
+//             const blurb = getBlurb(page)
+//             return (
+//               <li key={page.id}>
+//                 <div
+//                   style={{
+//                     display: 'flex',
+//                     flexDirection: 'column',
+//                   }}
+//                 >
+//                   <Link to={'/' + page.path}>{page.title}</Link>
+//                   <div
+//                     style={{
+//                       background: 'rgba(0,0,0,.1)',
+//                       marginLeft: '1em',
+//                     }}
+//                   >
+//                     {page.tags && ' Tags: ' + page.tags.join(`, `)}
+//                   </div>
+//                   <div style={{ marginLeft: '1em' }}>{blurb && blurb}</div>
+//                 </div>
+//               </li>
+//             )
+//           })}
+//       </ul>
+//     </div>
+//   )
+// }
+
+// export default Search
