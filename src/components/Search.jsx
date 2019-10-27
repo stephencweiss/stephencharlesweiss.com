4d3e366fe5482cd734441d34b654d11ab3dffa33
Stephen <stephen@remine.com>
2019-10-27 16:13:49 -0500

    clean up

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,24 +1,7 @@
-import React, { Component, useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'gatsby'
 import { Index } from 'elasticlunr'
 import { useDebounce } from 'use-debounce'
-
-// underscoreJS Debounce via David Walsh - https://davidwalsh.name/javascript-debounce-function
-function debounce(func, wait, immediate) {
-  var timeout
-  return function() {
-    var context = this,
-      args = arguments
-    var later = function() {
-      timeout = null
-      if (!immediate) func.apply(context, args)
-    }
-    var callNow = immediate && !timeout
-    clearTimeout(timeout)
-    timeout = setTimeout(later, wait)
-    if (callNow) func.apply(context, args)
-  }
-}
 
 function getBlurb(page) {
   return page.content.slice(0, 500).concat('...')
