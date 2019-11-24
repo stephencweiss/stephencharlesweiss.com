45a3050c1ed838d2d8077b62bd924b4a20047602
Stephen <stephen@remine.com>
2019-11-24 12:28:45 -0600

    wip - routing for blog

--- src/pages/blog.jsx
+++ src/pages/blog.jsx
@@ -3,30 +3,37 @@ import { graphql } from 'gatsby'
 import PostLink from '../components/PostLink'
 import Bio from '../components/Bio'
 import Layout from '../components/Layout'
+import SEO from '../components/SEO'
+import Header from '../components/Header'
 
-class BlogIndex extends React.Component {
-  render() {
-    const { data } = this.props
-    const siteTitle = data.site.siteMetadata.title
-    const posts = data.allMarkdownRemark.edges
 
-    return (
-      <Layout location={this.props.location} title={siteTitle}>
-        {posts.map(({ node }) => {
-          const { date, publish, title } = node.frontmatter
-          const { slug } = node.fields
-          return (
-            <div key={slug}>
-              <PostLink slug={slug} title={title} />
-              <small>{publish ? publish : date}</small>
-              <p dangerouslySetInnerHTML={{ __html: node.excerpt }} />
-            </div>
-          )
-        })}
-        <Bio />
-      </Layout>
-    )
-  }
+function BlogIndex(props) {
+  const { data } = props
+  const siteTitle = data.site.siteMetadata.title
+  const posts = data.allMarkdownRemark.edges
+
+  return (
+    <Layout location={props.location} title={siteTitle}>
+      <SEO
+          title="All posts"
+          keywords={[`blog`, `gatsby`, `javascript`, `react`]}
+        />
+        <Header />
+
+      {posts.map(({ node }) => {
+        const { date, publish, title } = node.frontmatter
+        const { slug } = node.fields
+        return (
+          <div key={slug}>
+            <PostLink slug={slug} title={title} />
+            <small>{publish ? publish : date}</small>
+            <p dangerouslySetInnerHTML={{ __html: node.excerpt }} />
+          </div>
+        )
+      })}
+      <Bio />
+    </Layout>
+  )
 }
 
 export default BlogIndex
@@ -38,10 +45,10 @@ export const pageQuery = graphql`
         title
       }
     }
-    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+    allMarkdownRemark(filter: { fields: { isPublished: { eq: true } } }) {
       edges {
         node {
-          excerpt
+          excerpt(format: MARKDOWN)
           fields {
             slug
           }
