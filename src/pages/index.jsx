2f58968519b92197971ab8708f862f7b89428a21
Stephen <stephen@remine.com>
2019-11-27 09:00:08 -0600

    extract sitemetadata to hook

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -9,45 +9,40 @@ import PostLink from '../components/PostLink'
 import sortPosts from '../utils/sortPosts'
 import getBlurb from '../utils/getBlurb'
 
+import useSiteMetadata from '../hooks/useSiteMetadata'
 
-class BlogIndex extends React.Component {
-  render() {
-    const { data } = this.props
-    const siteTitle = data.site.siteMetadata.title
-    const posts = data.allMarkdownRemark.edges.sort(sortPosts)
-    return (
-      <Layout location={this.props.location} title={siteTitle}>
-        <SEO
-          title="All posts"
-          keywords={[`blog`, `gatsby`, `javascript`, `react`]}
-        />
-        <Header />
-        {posts.map(({ node }) => {
-          const { date, publish, title } = node.frontmatter
-          const { slug } = node.fields
-          return (
-            <div key={slug}>
-              <PostLink slug={slug} title={title} />
-              <small>{publish ? publish : date}</small>
-              {getBlurb({content: node.excerpt, path: slug})}
-            </div>
-          )
-        })}
-        <Bio />
-      </Layout>
-    )
-  }
+function BlogIndex(props) {
+  const { data } = props
+  const { title } = useSiteMetadata()
+
+  const posts = data.allMarkdownRemark.edges.sort(sortPosts)
+  return (
+    <Layout location={props.location} title={title}>
+      <SEO
+        title="All posts"
+        keywords={[`blog`, `gatsby`, `javascript`, `react`]}
+      />
+      <Header />
+      {posts.map(({ node }) => {
+        const { date, publish, title } = node.frontmatter
+        const { slug } = node.fields
+        return (
+          <div key={slug}>
+            <PostLink slug={slug} title={title} />
+            <small>{publish ? publish : date}</small>
+            {getBlurb({ content: node.excerpt, path: slug })}
+          </div>
+        )
+      })}
+      <Bio />
+    </Layout>
+  )
 }
 
 export default BlogIndex
 
 export const pageQuery = graphql`
   query {
-    site {
-      siteMetadata {
-        title
-      }
-    }
     allMarkdownRemark(filter: { fields: { isPublished: { eq: true } } }) {
       edges {
         node {
