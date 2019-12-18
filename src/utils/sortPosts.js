a637cb88c7fb359188e447bce943acea4d8c0212
Stephen <stephencweiss@gmail.com>
2019-12-18 16:48:51 -0600

    refactor sortPosts for clarity

--- src/utils/sortPosts.js
+++ src/utils/sortPosts.js
@@ -1,15 +1,22 @@
-import dayjs from 'dayjs';
+import dayjs from 'dayjs'
 
 export default function sortPosts(a, b, order = 'DESC') {
   const { listDate: aDate } = a.node.fields
   const { listDate: bDate } = b.node.fields
+
   if (!aDate || !bDate) {
-      throw new Error(`sortPosts is broken! Check frontmatter for missing date fields!`)
+    throw new Error(
+      `sortPosts is broken! Check frontmatter for missing date fields! ${JSON.stringify(
+        { a, b },
+        null,
+        4
+      )}`
+    )
   }
 
   if (order === 'ASC') {
-    return dayjs(aDate).isAfter(dayjs(bDate)) ? 1 : -1
+    return dayjs(aDate).isBefore(dayjs(bDate)) ? -1 : 1
   } else if (order === 'DESC') {
-    return dayjs(aDate).isAfter(dayjs(bDate)) ? -1 : 1
+    return dayjs(aDate).isBefore(dayjs(bDate)) ? 1 : -1
   }
 }
