c8236e07c05cc0cb191fda62484a63488440a09c
Stephen <stephencweiss@gmail.com>
2019-12-19 09:36:01 -0600

    add header options

---
+++ src/components/Headers/EntryHeader.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+function EntryHeader(props) {
+  const { title } = props
+  return (
+    <h3
+      style={{
+        fontFamily: `Montserrat, sans-serif`,
+        marginTop: 0,
+      }}
+    >
+      <Link
+        style={{
+          boxShadow: `none`,
+          textDecoration: `none`,
+          color: `inherit`,
+        }}
+        to={`/`}
+      >
+        {title}
+      </Link>
+    </h3>
+  )
+}
+
+export default EntryHeader
---
+++ src/components/Headers/index.js
@@ -0,0 +1,2 @@
+export {default as EntryHeader} from './EntryHeader'
+export {default as RootHeader} from './RootHeader'
