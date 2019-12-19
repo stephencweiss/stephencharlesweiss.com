a71425c5b718d6c4b2dd186459e7147a7d078661
Stephen <stephencweiss@gmail.com>
2019-12-19 09:40:07 -0600

    replace search import

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,25 +4,24 @@ import { graphql } from 'gatsby'
 import Bio from '../components/Bio'
 import Layout from '../components/Layout'
 import SEO from '../components/SEO'
-import Header from '../components/Header'
 import PostLink from '../components/PostLink'
 import sortPosts from '../utils/sortPosts'
 import getBlurb from '../utils/getBlurb'
 
 import useSiteMetadata from '../hooks/useSiteMetadata'
 
-function BlogIndex(props) {
+function MainIndex(props) {
   const { data } = props
   const { title } = useSiteMetadata()
-
   const posts = data.allMarkdownRemark.edges.sort(sortPosts)
+
   return (
     <Layout location={props.location} title={title}>
       <SEO
         title="All posts"
         keywords={[`blog`, `gatsby`, `javascript`, `react`]}
       />
-      <Header />
+      <Search />
       {posts.map(({ node }) => {
         const { title } = node.frontmatter
         const { listDate, slug } = node.fields
@@ -39,7 +38,7 @@ function BlogIndex(props) {
   )
 }
 
-export default BlogIndex
+export default MainIndex
 
 export const pageQuery = graphql`
   query indexBlogQuery {
