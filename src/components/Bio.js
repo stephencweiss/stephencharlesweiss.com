7696d12319fee6ca2c128a6f854e49cf1436420b
Stephen <stephencweiss@gmail.com>
2019-12-19 09:39:12 -0600

    wip - style bio

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { StaticQuery, graphql } from 'gatsby'
 import Image from 'gatsby-image'
 
-import { rhythm } from '../utils/typography'
+// TODO: Style
 
 function Bio() {
   return (
@@ -14,15 +14,15 @@ function Bio() {
           <div
             style={{
               display: `flex`,
-              marginBottom: rhythm(2.5),
-              marginTop: rhythm(2.5),
+            //   marginBottom: rhythm(2.5),
+            //   marginTop: rhythm(2.5),
             }}
           >
             <Image
               fixed={data.avatar.childImageSharp.fixed}
               alt={author}
               style={{
-                marginRight: rhythm(1 / 2),
+                // marginRight: rhythm(1 / 2),
                 marginBottom: 0,
                 minWidth: 50,
                 borderRadius: `100%`,
