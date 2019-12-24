6489d0e4b7ab910536bfcd63ed808a94f8ecea9c
Stephen <stephencweiss@gmail.com>
2019-12-23 22:02:00 -0800

    wip theme

--- src/theme/theme.js
+++ src/theme/theme.js
@@ -2,24 +2,38 @@ import { css } from 'styled-components'
 import theme from 'styled-theming'
 
 const whiteish = `#F6F7EB`
-const blackish = '#090314'
+const solarized = `#fdf6e3`
+const highlighted = `#feb`
+const blackish = `#090314`
+const reddish = `rgba(255, 109, 91, 0.99)`
 
 export default theme('mode', {
   light: css`
     background-color: ${whiteish};
     color: ${blackish};
     * + ::selection {
-      background: rgba(255, 109, 91, 0.99) none;
+      background: ${reddish} none;
       text-shadow: none;
       color: ${whiteish};
     }
-  `,
+
+    .gatsby-code-title {
+        background-color: ${reddish};
+        color: ${blackish};
+    }
+}
+`,
   dark: css`
     background-color: ${blackish};
     color: ${whiteish};
     * + ::selection {
-      background: rgba(255, 109, 91, 0.99) none;
+      background: ${reddish} none;
       text-shadow: none;
+      color: ${blackish};
+    }
+
+    .gatsby-code-title {
+      background-color: ${reddish};
       color: ${blackish};
     }
   `,
