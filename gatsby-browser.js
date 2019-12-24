3a2257dcfc19fd2817dcc86e06812257d3a60cc5
Stephen <stephencweiss@gmail.com>
2019-12-23 21:37:00 -0800

    syntax highlighting

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,3 +1,6 @@
 import wrapWithProvider from './src/components/wrapWithProvider'
+require("prismjs/themes/prism-solarizedlight.css")
+require("prismjs/plugins/line-numbers/prism-line-numbers.css")
+require("prismjs/plugins/command-line/prism-command-line.css")
 
 export const wrapRootElement = wrapWithProvider
