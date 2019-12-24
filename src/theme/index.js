5b0570c0e6dd5a01c885093067bf858cc88a2bba
Stephen <stephencweiss@gmail.com>
2019-12-23 19:50:37 -0800

    global styles

---
+++ src/theme/index.js
@@ -0,0 +1,5 @@
+import theme from './theme'
+import GlobalStyle from './GlobalStyle'
+import { space, spaceByIndex } from './space'
+export default theme
+export { GlobalStyle, space, spaceByIndex }
