3a72c5a9af6a239ed46ec4278ee5c1b46f3acbf2
Stephen <stephencweiss@gmail.com>
2019-12-24 20:34:56 -0800

    breakdown css imports

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,6 +1,6 @@
 import wrapWithProvider from './src/components/wrapWithProvider'
-require("prismjs/themes/prism-solarizedlight.css")
-require("prismjs/plugins/line-numbers/prism-line-numbers.css")
-require("prismjs/plugins/command-line/prism-command-line.css")
+import "./src/stylesheets/_reset.css"
+import "./src/stylesheets/global.css"
+import "./src/stylesheets/code.css"
 
 export const wrapRootElement = wrapWithProvider
