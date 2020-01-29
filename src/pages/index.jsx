215173f6ee2d1ae9bb58610123287dbfb834dc5a
Stephen <stephencweiss@gmail.com>
2020-01-29 08:36:04 -0600

    fix index now that pagination is working

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,20 +1,19 @@
 import React from 'react'
-import { graphql, Link } from 'gatsby'
-
-import Bio from '../components/Bio'
-import Layout from '../components/Layout'
-import { Search } from '../components/Search'
-import SEO from '../components/SEO'
-import PostLink from '../components/PostLink'
-import sortPosts from '../utils/sortPosts'
-import getBlurb from '../utils/getBlurb'
-import { ENTRIES_PER_PAGE } from '../constants'
+import { graphql } from 'gatsby'
+import {
+  EntryCard,
+  Bio,
+  Layout,
+  Search,
+  SEO,
+  PageNavigation,
+} from '../components'
 import useSiteMetadata from '../hooks/useSiteMetadata'
 
 function MainIndex(props) {
   const { data } = props
   const { title } = useSiteMetadata()
-  const posts = data.allMarkdownRemark.edges//.sort(sortPosts)
+  const posts = data.allMarkdownRemark.edges //.sort(sortPosts)
 
   return (
     <Layout location={props.location} title={title}>
@@ -23,25 +22,10 @@ function MainIndex(props) {
         keywords={[`blog`, `gatsby`, `javascript`, `react`]}
       />
       <Search />
-      {posts.map(({ node }) => {
-        const { title } = node.frontmatter
-        const { listDate, slug } = node.fields
-        return (
-          <div key={slug}>
-            <PostLink slug={slug} title={title} />
-            <small>{listDate}</small>
-            {getBlurb({ content: node.excerpt, path: slug })}
-          </div>
-        )
-      })}
-
-      {/**
-       * TODO:
-       * 1. Style better
-       * 2. Go directly to the _next_ set of posts (i.e. if this shows 6, show the _second_ 6)
-       * 3. Retire the `/blog` page?
-       */}
-      <Link to={`/blog/1`}>See More</Link>
+      {posts.map(({ node }) => (
+        <EntryCard key={node.frontmatter.slug} node={node} />
+      ))}
+      <PageNavigation next={`/blog/1`} />
       <Bio />
     </Layout>
   )
@@ -55,16 +39,14 @@ export const pageQuery = graphql`
       filter: {
         fields: { isPublished: { eq: true }, sourceInstance: { eq: "blog" } }
       }
-      # NOTE: limit is the same as ENTRIES_PER_PAGE
-      # Cannot use string interpolation in graphql however.
-      # limit: 6
+      limit: 6 # NOTE: value for limit is the same as ENTRIES_PER_PAGE; cannot string interpolate w/in graphql function
     ) {
       edges {
         node {
           excerpt(format: PLAIN)
           fields {
             slug
-            # listDate(formatString: "MMMM DD, YYYY")
+            listDate
           }
           frontmatter {
             title
