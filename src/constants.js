efa8059ed47ef2990a23af4db77ea17ae01ed0fc
Stephen <stephencweiss@gmail.com>
2020-01-29 08:35:42 -0600

    Correct export
    Was exporting Fake_time but referencing fake_start - led to silent failures in createnodefield

--- src/constants.js
+++ src/constants.js
@@ -2,9 +2,9 @@ const dayjs = require(`dayjs`)
 
 const ENTRIES_PER_PAGE = 6
 const BUILD_TIME = dayjs()
-const FAKE_TIME = dayjs('1900-01-01')
+const FAKE_START = dayjs('1900-01-01')
 const TODAY = dayjs()
   .format('YYYY-MM-DD')
   .toString()
 
-module.exports = { BUILD_TIME, ENTRIES_PER_PAGE, FAKE_TIME, TODAY }
+module.exports = { BUILD_TIME, ENTRIES_PER_PAGE, FAKE_START, TODAY }
