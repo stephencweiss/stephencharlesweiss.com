01f23e117d4ffeb7f38e416b6659e6fc3d604502
Stephen <stephencweiss@gmail.com>
2020-03-03 08:35:25 -0600

    feat: add embedder

--- gatsby-config.js
+++ gatsby-config.js
@@ -125,6 +125,7 @@ module.exports = {
               wrapperStyle: `margin-bottom: 1.0725rem`,
             },
           },
+          { resolve: 'gatsby-remark-embedder' },
         ],
       },
     },
