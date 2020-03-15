451c7fe6ad16207b9f3d88daa96198c6a732dfbc
Stephen <stephen.c.weiss@gmail.com>
2020-03-15 12:15:25 -0500

    feat: remove theming
    This was a half-baked approach that wasn't accessible (a11y) or fully
    implemented.
    
    Removing to restart with a fresh start and hopefully with a clearer
    idea for what i want in terms of a style.

--- src/components/BlogExcerptComposite.jsx
+++ src/components/BlogExcerptComposite.jsx
@@ -1,31 +1,13 @@
-import React, { useContext } from 'react'
+import React from 'react'
 import { PostLink, PostDate } from '.'
 import { getBlurb } from '../utils'
 import styled, { ThemeContext } from 'styled-components'
-import { blackish, solarized } from '../theme/colors'
 
-
-/**
- * WIP - TODO: Need to figure out how to get themes in!
- */
 const Card = styled.div`
-  border: 2px solid ${({ theme }) => theme.border};
+  border: 2px solid rgba(0,0,0,.75);
   margin: 1em 0;
-  background: ${({ theme }) => {
-    return theme.border
-  }};
+  background: rgba(255,255,255,.85);
 `
-const theme = {
-  light: {
-    background: solarized,
-    border: blackish,
-  },
-  dark: {
-    background: blackish,
-    border: solarized,
-  },
-}
-
 const ContentWrapper = styled.div`
   padding: 0.5em;
 `
@@ -34,9 +16,8 @@ export function EntryCard({ node }) {
   const { title } = node.frontmatter
   const { listDate, slug } = node.fields
   const { excerpt } = node
-  const themeContext = useContext(ThemeContext)
   return (
-    <Card theme={themeContext.mode}>
+    <Card>
       <ContentWrapper>
         <PostLink slug={slug} title={title} />
         <PostDate>{listDate}</PostDate>
