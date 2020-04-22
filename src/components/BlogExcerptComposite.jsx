611d6bc8b8e789ead3906eb88c0dae26c40ef805
Stephen <stephen.c.weiss@gmail.com>
2020-04-22 11:26:48 -0500

    feat: remove card outline
    moving away from "cards" for my blog into a more typical list of blog
    posts.

--- src/components/BlogExcerptComposite.jsx
+++ src/components/BlogExcerptComposite.jsx
@@ -1,28 +1,24 @@
 import React from 'react'
-import { PostLink, PostDate } from '.'
+import { PostTitleLink, PostDate } from '.'
 import { getBlurb } from '../utils'
-import styled, { ThemeContext } from 'styled-components'
+import styled from 'styled-components'
 
-const Card = styled.div`
-  border: 2px solid rgba(0,0,0,.75);
-  margin: 1em 0;
-  background: rgba(255,255,255,.85);
-`
 const ContentWrapper = styled.div`
-  padding: 0.5em;
+    padding-bottom: 1.5em;
+    :last-of-type{
+        padding-bottom:0em;
+    }
 `
 
 export function EntryCard({ node }) {
-  const { title } = node.frontmatter
-  const { listDate, slug } = node.fields
-  const { excerpt } = node
-  return (
-    <Card>
-      <ContentWrapper>
-        <PostLink slug={slug} title={title} />
-        <PostDate>{listDate}</PostDate>
-        {getBlurb({ content: excerpt, path: slug })}
-      </ContentWrapper>
-    </Card>
-  )
+    const { title } = node.frontmatter
+    const { listDate, slug } = node.fields
+    const { excerpt } = node
+    return (
+        <ContentWrapper>
+            <PostTitleLink slug={slug} title={title} />
+            <PostDate>{listDate}</PostDate>
+            {getBlurb({ content: excerpt, path: slug })}
+        </ContentWrapper>
+    )
 }
