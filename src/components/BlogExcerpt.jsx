e1becf3230f8b4cde6b1ea86634d51954243de10
Stephen <stephen.c.weiss@gmail.com>
2020-04-22 11:46:20 -0500

    feat: add reading time to excerpts

--- src/components/BlogExcerpt.jsx
+++ src/components/BlogExcerpt.jsx
@@ -5,19 +5,34 @@ import styled from 'styled-components'
 
 const ContentWrapper = styled.div`
     padding-bottom: 1.5em;
-    :last-of-type{
-        padding-bottom:0em;
+    :last-of-type {
+        padding-bottom: 0em;
     }
+`
+
+export const PostDetails = styled.h5`
+    display: inline;
+    color: #606060;
+`
+
+export const DetailsWrapper = styled.div`
+    margin: 0.5rem 0 0;
 `
 
 export function BlogExcerpt({ node }) {
     const { title } = node.frontmatter
-    const { listDate, slug } = node.fields
+    const { listDate, slug, readingTime } = node.fields
+    const { words, text } = readingTime
     const { excerpt } = node
     return (
         <ContentWrapper>
             <PostTitleLink slug={slug} title={title} />
-            <PostDate>{listDate}</PostDate>
+            <DetailsWrapper>
+                <PostDetails>{listDate}</PostDetails>&nbsp;|&nbsp;
+                <PostDetails>
+                    {text}, {words} words
+                </PostDetails>
+            </DetailsWrapper>
             {getBlurb({ content: excerpt, path: slug })}
         </ContentWrapper>
     )
