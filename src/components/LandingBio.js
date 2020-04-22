d456e6133783c417201e3014b0248c88ea8fdc03
Stephen <stephen.c.weiss@gmail.com>
2020-04-22 09:31:45 -0500

    feat: make landing page simple and explanatory

---
+++ src/components/LandingBio.js
@@ -0,0 +1,64 @@
+import React from "react"
+import PropTypes from "prop-types"
+import { StaticQuery, graphql } from "gatsby"
+import styled from "styled-components"
+
+const Container = styled.div`
+  text-align: center;
+`
+
+const OuterContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: row;
+  height: 78vh;
+`
+
+const Description = styled.p`
+  padding: 0;
+  margin-bottom: 1rem;
+  font-size: 1.4rem;
+`
+
+const NameHeader = styled.h1`
+  font-size: 3.5rem;
+  margin-bottom: 0;
+`
+
+const LandingBio = () => (
+  <StaticQuery
+    query={LandingSiteTitleQuery}
+    render={data => (
+      <OuterContainer>
+        <Container>
+          <NameHeader>{data.site.siteMetadata.title}</NameHeader>
+          <Description>{data.site.siteMetadata.description}</Description>
+        </Container>
+      </OuterContainer>
+    )}
+  />
+)
+
+NameHeader.propTypes = {
+  siteTitle: PropTypes.string,
+  description: PropTypes.string,
+}
+
+NameHeader.defaultProps = {
+  siteTitle: ``,
+  description: ``,
+}
+
+export default LandingBio
+
+const LandingSiteTitleQuery = graphql`
+      query LandingSiteTitleQuery {
+        site {
+          siteMetadata {
+            title
+            description
+          }
+        }
+      }
+    `
