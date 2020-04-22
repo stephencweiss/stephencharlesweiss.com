e989f28bb2c9a3db7b1f8602481e718243db7c18
Stephen <stephen.c.weiss@gmail.com>
2020-04-22 09:36:40 -0500

    style: general layout fix
    content takes up center, footer is stuck to bottom (and centered).

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,13 +1,22 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { Header } from '../components/Headers'
 import styled from 'styled-components'
 
 const Paper = styled.div`
     min-height: 100vh;
+    display: flex;
+    flex-direction: column;
 `
 const Wrapper = styled.div`
     max-width: 40em;
     margin: auto;
+    flex: 1;
+`
+const Footer = styled.div`
+    display: flex;
+    max-height: 30px;
+    margin: 1rem;
+    justify-content: center;
 `
 
 export function Layout(props) {
@@ -19,11 +28,11 @@ export function Layout(props) {
             <Wrapper>
                 <Header title={title} root={location.pathname === rootPath} />
                 {children}
-                <footer>
-                    © {new Date().getFullYear()}&nbsp; Built with ❤️ using&nbsp;
-                    <a href="https://www.gatsbyjs.org">Gatsby</a>
-                </footer>
             </Wrapper>
+            <Footer>
+                © {new Date().getFullYear()}&nbsp; Built with ❤️ using&nbsp;
+                <a href="https://www.gatsbyjs.org">Gatsby</a>
+            </Footer>
         </Paper>
     )
 }
