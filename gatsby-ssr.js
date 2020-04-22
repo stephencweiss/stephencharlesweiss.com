5f1cb9914f409395f97a2cdc78ff42e3835250d9
Stephen <stephen.c.weiss@gmail.com>
2020-04-22 09:23:27 -0500

    feat: restore ssr to default
    i'm not getting the benefits from the ssr as I am sure i configured it
    incorrectly. so for now, removing my changes.

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,3 +1,7 @@
-import wrapWithProvider from './src/components/wrapWithProvider'
+/**
+ * Implement Gatsby's SSR (Server Side Rendering) APIs in this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/ssr-apis/
+ */
 
-export const wrapRootElement = wrapWithProvider
+// You can delete this file if you're not using it
