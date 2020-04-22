f6cbf44b4baa36883ccea8b37891dfae3629e894
Stephen <stephen.c.weiss@gmail.com>
2020-04-22 17:57:19 -0500

    feat: update details on blog list and entry

--- src/templates/BlogEntry.js
+++ src/templates/BlogEntry.js
@@ -2,65 +2,72 @@ import React from 'react'
 import { graphql } from 'gatsby'
 import styled from 'styled-components'
 import {
-  Bio,
-  Layout,
-  PostDate,
-  SEO,
-  Title,
-  PostNavigation,
+    Bio,
+    Layout,
+    PostDetails,
+    SEO,
+    Title,
+    PostNavigation,
 } from '../components'
 import { useSiteMetadata } from '../hooks'
 
 const Entry = styled.div`
-  #resources + ul > li,
-  #footnotes + ul > li {
-    list-style: none;
-  }
+    #resources + ul > li,
+    #footnotes + ul > li {
+        list-style: none;
+    }
 `
 
 function EntryTemplate(props) {
-  const entry = props.data.markdownRemark
-  const { title: siteTitle } = useSiteMetadata()
-  const { previous, next } = props.pageContext
-  const { date, publish, title } = entry.frontmatter
-    console.log({previous, next})
-  return (
-    <Layout location={props.location} title={siteTitle}>
-      <SEO title={title} description={entry.excerpt} />
-      <div style={{ display: 'flex', flexDirection: 'column' }}>
-        <PostDate>{publish ? publish : date}</PostDate>
-        <Title>{title}</Title>
-      </div>
-      <Entry
-        className={'entry'}
-        dangerouslySetInnerHTML={{ __html: entry.html }}
-      />
-      <hr />
-      <Bio />
-      <PostNavigation previous={previous} next={next} />
-    </Layout>
-  )
+    const entry = props.data.markdownRemark
+    const { title: siteTitle } = useSiteMetadata()
+    const { previous, next } = props.pageContext
+    const { title } = entry.frontmatter
+    const { listDate, readingTime } = entry.fields
+    const { text:estimate, words:wordCount } = readingTime
+    return (
+        <Layout location={props.location} title={siteTitle}>
+            <SEO title={title} description={entry.excerpt} />
+            <div style={{ display: 'flex', flexDirection: 'column' }}>
+                <Title>{title}</Title>
+                <PostDetails date={listDate} estimate={estimate} wordCount={wordCount}/>
+            </div>
+            <Entry
+                className={'entry'}
+                dangerouslySetInnerHTML={{ __html: entry.html }}
+            />
+            <hr />
+            <Bio />
+            <PostNavigation previous={previous} next={next} />
+        </Layout>
+    )
 }
 
 export default EntryTemplate
 
 export const pageQuery = graphql`
-  query EntryBySlug($slug: String!) {
-    site {
-      siteMetadata {
-        title
-        author
-      }
+    query EntryBySlug($slug: String!) {
+        site {
+            siteMetadata {
+                title
+                author
+            }
+        }
+        markdownRemark(fields: { slug: { eq: $slug } }) {
+            id
+            html
+            frontmatter {
+                title
+                date(formatString: "MMMM DD, YYYY")
+                publish(formatString: "MMMM DD, YYYY")
+            }
+            fields {
+                listDate
+                readingTime {
+                    text
+                    words
+                }
+            }
+        }
     }
-    markdownRemark(fields: { slug: { eq: $slug } }) {
-      id
-      excerpt(pruneLength: 160)
-      html
-      frontmatter {
-        title
-        date(formatString: "MMMM DD, YYYY")
-        publish(formatString: "MMMM DD, YYYY")
-      }
-    }
-  }
 `
--- src/templates/BlogList.js
+++ src/templates/BlogList.js
@@ -2,78 +2,80 @@ import React from 'react'
 import { graphql } from 'gatsby'
 import styled from 'styled-components'
 import {
-  Bio,
-  BlogExcerpt,
-  Layout,
-  PageNavigation,
-  SEO,
-  Search,
+    Bio,
+    BlogExcerpt,
+    Layout,
+    PageNavigation,
+    SEO,
+    Search,
 } from '../components'
 import { useSiteMetadata } from '../hooks'
 
 // TODO: figure out if this is used/helpful - think it conflicts with the <layout>
 const Content = styled.div`
-  margin: 0 auto;
-  max-width: 860px;
-  padding: 1.45rem 1.0875rem;
+    margin: 0 auto;
+    max-width: 860px;
+    padding: 1.45rem 1.0875rem;
 `
 
 function BlogList(props) {
-  const { data } = props
-  console.log({props})
-  const { previousPage, nextPage, currentPage } = props.pageContext
-  const { title: siteTitle } = useSiteMetadata()
-  const posts = data.allMarkdownRemark.edges
+    const { data } = props
+    const { previousPage, nextPage, currentPage } = props.pageContext
+    const { title: siteTitle } = useSiteMetadata()
+    const posts = data.allMarkdownRemark.edges
 
-  return (
-    <Layout location={props.location} title={siteTitle}>
-      <SEO
-        title="All posts"
-        keywords={[`blog`, `gatsby`, `javascript`, `react`]}
-      />
-      <Content>
-        <h1>Blog, page: {currentPage + 1}</h1>
+    return (
+        <Layout location={props.location} title={siteTitle}>
+            <SEO
+                title="All posts"
+                keywords={[`blog`, `gatsby`, `javascript`, `react`]}
+            />
+            <Content>
+                <h1>Blog, page: {currentPage + 1}</h1>
 
-      <Search />
-      {posts.map(({ node }) => (
-          <BlogExcerpt key={node.frontmatter.slug} node={node} />
-          ))}
+                <Search />
+                {posts.map(({ node }) => (
+                    <BlogExcerpt key={node.frontmatter.slug} node={node} />
+                ))}
 
-      <PageNavigation previous={previousPage} next={nextPage} />
-          </Content>
-      <Bio />
-    </Layout>
-  )
+                <PageNavigation previous={previousPage} next={nextPage} />
+            </Content>
+            <Bio />
+        </Layout>
+    )
 }
 
 export default BlogList
 
 export const pageQuery = graphql`
-  query getPaginatedBlogData($limit: Int!, $skip: Int!) {
-    allMarkdownRemark(
-      filter: {
-        fields: { isPublished: { eq: true }, sourceInstance: { eq: "blog" } }
-      }
-      sort: { order: [DESC], fields: [fields___listDate] }
-      limit: $limit
-      skip: $skip
-    ) {
-      edges {
-        node {
-          excerpt(format: PLAIN)
-          fields {
-            slug
-            listDate
-            readingTime {
-              words
-              text
+    query getPaginatedBlogData($limit: Int!, $skip: Int!) {
+        allMarkdownRemark(
+            filter: {
+                fields: {
+                    isPublished: { eq: true }
+                    sourceInstance: { eq: "blog" }
+                }
             }
-          }
-          frontmatter {
-            title
-          }
+            sort: { order: [DESC], fields: [fields___listDate] }
+            limit: $limit
+            skip: $skip
+        ) {
+            edges {
+                node {
+                    excerpt(format: PLAIN)
+                    fields {
+                        slug
+                        listDate
+                        readingTime {
+                            words
+                            text
+                        }
+                    }
+                    frontmatter {
+                        title
+                    }
+                }
+            }
         }
-      }
     }
-  }
 `
