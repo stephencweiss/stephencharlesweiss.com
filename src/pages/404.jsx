0fd3b4b61944fdcb75fb75b7fe584067a8eeda00
Stephen <stephen.c.weiss@gmail.com>
2020-04-23 16:54:49 -0500

    refactor: convert to functional component

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,18 +1,15 @@
 import React from 'react'
-
 import Layout from '../components/Layout'
 import SEO from '../components/SEO'
 
-class NotFoundPage extends React.Component {
-  render() {
+function NotFoundPage() {
     return (
-      <Layout location={this.props.location}>
-        <SEO title="404: Not Found" />
-        <h1>Not Found</h1>
-        <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-      </Layout>
+        <Layout>
+            <SEO title="404: Not Found" />
+            <h1>Not Found</h1>
+            <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+        </Layout>
     )
-  }
 }
 
 export default NotFoundPage
