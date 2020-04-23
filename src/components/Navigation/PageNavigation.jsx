8672160a668cff7a55cf7b27e416d8a51013900e
Stephen <stephen.c.weiss@gmail.com>
2020-04-22 21:34:02 -0500

    style: make pageNav stylish
    use the styled link on the page navigation

--- src/components/Navigation/PageNavigation.jsx
+++ src/components/Navigation/PageNavigation.jsx
@@ -1,40 +1,41 @@
 import React from 'react'
 import styled from 'styled-components'
-import { Link } from 'gatsby'
+import { StyledLink } from '../index'
 
 const List = styled.ul`
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: space-around;
-  list-style: none;
-  padding: 0;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-around;
+    list-style: none;
+    padding: 0;
+    margin: 1rem 0;
 `
 
 const ListItem = styled.li`
-  flex: 1;
-  text-align: ${({ align }) => (align ? align : '')};
+    flex: 1;
+    text-align: ${({ align }) => (align ? align : '')};
 `
 
 function PageNavigation(props) {
-  const { previous, next } = props
-  return (
-    <List>
-      {previous && (
-        <ListItem align={'left'}>
-          <Link to={previous} rel="prev">
-            &lArr; Previous Page
-          </Link>
-        </ListItem>
-      )}
-      {next && (
-        <ListItem align={'right'}>
-          <Link to={next} rel="next">
-            Next Page &rArr;
-          </Link>
-        </ListItem>
-      )}
-    </List>
-  )
+    const { previous, next } = props
+    return (
+        <List>
+            {previous && (
+                <ListItem align={'left'}>
+                    <StyledLink to={previous} rel="prev">
+                        &lArr; Previous Page
+                    </StyledLink>
+                </ListItem>
+            )}
+            {next && (
+                <ListItem align={'right'}>
+                    <StyledLink to={next} rel="next">
+                        Next Page &rArr;
+                    </StyledLink>
+                </ListItem>
+            )}
+        </List>
+    )
 }
 
 export default PageNavigation
