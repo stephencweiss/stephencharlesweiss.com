48549cfe1bf68810b837441b5ee9b12110b89177
Stephen <stephen.c.weiss@gmail.com>
2020-04-22 19:00:44 -0500

    style: remove highlight on hover/focus
    this allows the link to come through without conflict

--- src/components/PostTitle.jsx
+++ src/components/PostTitle.jsx
@@ -2,16 +2,24 @@ import React from 'react'
 import { StyledLink } from '.'
 import styled, { css } from 'styled-components'
 
+const HighlighterGradient = css`
+linear-gradient(
+    -100deg,
+    rgba(255, 250, 150, 0.15),
+    rgba(255, 250, 150, 0.8) 100%,
+    rgba(255, 250, 150, 0.25)
+)`
+
 export const MarkerStyle = css`
     display: inline;
     border-radius: 1em 0 1em 0;
     padding: 0.5em;
-    background: linear-gradient(
-        -100deg,
-        rgba(255, 250, 150, 0.15),
-        rgba(255, 250, 150, 0.8) 100%,
-        rgba(255, 250, 150, 0.25)
-    );
+    background: ${HighlighterGradient};
+    :focus,
+    :focus-within,
+    :hover {
+        background: transparent;
+    }
 `
 
 export const MarkerHeader = styled.h3`
