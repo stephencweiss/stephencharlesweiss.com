77feb1d72b608f8b64ddd8c333352ffb0f602add
Stephen <stephen.c.weiss@gmail.com>
2020-05-08 18:41:39 -0500

    feat: add drop letter

--- src/templates/BlogEntry.js
+++ src/templates/BlogEntry.js
@@ -11,11 +11,24 @@ import {
 } from '../components'
 
 const Entry = styled.div`
-    #resources + ul > li,
-    #footnotes + ul > li {
-        list-style: none;
-    }
     padding: 0 0.5em;
+
+    & > p:first-of-type::first-letter {
+        color: #c69c6d;
+        float: left;
+        font-family: Georgia;
+        font-size: 5em;
+        margin: 0 0.2em 0 0;
+        line-height: 0.5;
+        text-transform: lowercase;
+    }
+`
+
+const PostHeaderBlock = styled.div`
+    display: flex;
+    flex-direction: column;
+    margin: 0 0 2rem 0;
+    padding: 0;
 `
 
 function EntryTemplate(props) {
@@ -27,14 +40,14 @@ function EntryTemplate(props) {
     return (
         <Layout>
             <SEO title={title} description={entry.excerpt} />
-            <div style={{ display: 'flex', flexDirection: 'column' }}>
-                <PostTitle title={title}/>
+            <PostHeaderBlock>
+                <PostTitle title={title} />
                 <PostDetails
                     date={listDate}
                     estimate={estimate}
                     wordCount={wordCount}
                 />
-            </div>
+            </PostHeaderBlock>
             <Entry
                 className={'entry'}
                 dangerouslySetInnerHTML={{ __html: entry.html }}
