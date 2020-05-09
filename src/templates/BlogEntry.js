2c1af5b0e1e6bb81ff964262bd0fcc845896a51d
Stephen <stephen.c.weiss@gmail.com>
2020-05-08 20:03:40 -0500

    style: progressive enhancement on firstletter
    using an @support query, i can determine if the modern css will work or
    not.
    
    That's pretty convenient!

--- src/templates/BlogEntry.js
+++ src/templates/BlogEntry.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { graphql } from 'gatsby'
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 import {
     Bio,
     Layout,
@@ -10,18 +10,33 @@ import {
     PostTitle,
 } from '../components'
 
+const classicFirstLetterStyle = css`
+    float: left;
+    font-size: 3.5em;
+    font-weight: bold;
+    margin: 0 0.2em 0 0;
+    line-height: 0.75;
+`
+
+const modernFirstLetterStyle = css`
+    -webkit-initial-letter: 3 2;
+    initial-letter: 3 2;
+`
+
 const Entry = styled.div`
     padding: 0 0.5em;
 
     & > p:first-of-type::first-letter {
         color: rgb(70, 70, 70);
-        float: left;
         font-family: Georgia, serif;
-        font-size: 3.5em;
-        font-weight: bold;
-        margin: 0 0.2em 0 0;
-        line-height: 0.75;
         text-transform: lowercase;
+        @supports not (initial-letter: 1 and -webkit-initial-letter: 1) {
+            ${classicFirstLetterStyle}
+        }
+
+        @supports (initial-letter: 1) or (-webkit-initial-letter: 1) {
+            ${modernFirstLetterStyle}
+        }
     }
 `
 
